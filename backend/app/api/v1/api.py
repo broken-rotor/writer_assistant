d5970171d591efd72cd5838663ee725dfa1bd882
HappySprite <belzaru17@gmail.com>
2025-10-12 12:19:57 +0100

    Remove old api.

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import generation, ai_generation
+from app.api.v1.endpoints import ai_generation
 
 api_router = APIRouter()
-api_router.include_router(generation.router, prefix="/generate", tags=["generation"])
 api_router.include_router(ai_generation.router, tags=["ai-generation"])
